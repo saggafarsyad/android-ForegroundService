5ea8d524361dbbaf50798233d5d2b8c0b42acd79
Saggaf Arsyad <saggaf@area54labs.net>
2015-05-14 10:50:14 +0700

    Set Activity LaunchMode to SingleTask. Add LogCat

--- app/src/main/java/com/saggafarsyad/helloapp/HelloService.java
+++ app/src/main/java/com/saggafarsyad/helloapp/HelloService.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -14,18 +15,12 @@ import android.widget.Toast;
  */
 public class HelloService extends Service
 {
-    // Foreground status
-    private int mState;
     public static final int STATE_START = 1;
     public static final int STATE_STOP = 0;
-
+    private final String LOG_TAG = "HelloService";
     private final IBinder mBinder = new LocalBinder();
-
-    public class LocalBinder extends Binder {
-        public HelloService getService() {
-            return HelloService.this;
-        }
-    }
+    // Foreground status
+    private int mState;
 
     public HelloService() {}
 
@@ -39,6 +34,7 @@ public class HelloService extends Service
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(LOG_TAG, "onStartCommand");
         mState = STATE_STOP;
 
         return START_STICKY;
@@ -46,8 +42,11 @@ public class HelloService extends Service
 
     @Override
     public void onDestroy() {
+        Log.i(LOG_TAG, "onDestroy");
+
         // Stop foreground if still running
         if (mState == STATE_START) {
+            Log.i(LOG_TAG, "Stopping Service...");
             stop();
         }
 
@@ -56,20 +55,26 @@ public class HelloService extends Service
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.i(LOG_TAG, "onBind");
         return mBinder;
     }
 
     @Override
     public void onRebind(Intent intent) {
+        Log.i(LOG_TAG, "onRebind");
+        Log.i(LOG_TAG, "State : " + mState);
         super.onRebind(intent);
 
         // @todo: Return last state
-        mState = STATE_START;
+//        mState = STATE_START;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        // Allow ReBinding
+        Log.i(LOG_TAG, "onUnbind");
+        Log.i(LOG_TAG, "State : " + mState);
+
+        // Return true to allow reBinding
         return true;
     }
 
@@ -96,7 +101,7 @@ public class HelloService extends Service
                 this,
                 0,
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
+                PendingIntent.FLAG_NO_CREATE
         );
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
@@ -106,5 +111,11 @@ public class HelloService extends Service
                 .setContentIntent(pendingIntent);
 
        return mBuilder.build();
+    }
+
+    public class LocalBinder extends Binder {
+        public HelloService getService() {
+            return HelloService.this;
+        }
     }
 }
--- app/src/main/java/com/saggafarsyad/helloapp/MainActivity.java
+++ app/src/main/java/com/saggafarsyad/helloapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.saggafarsyad.helloapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,8 +12,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new MainActivityFragment())
+                    .commit();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
